c8d74bb6141246fed48a1d8bd26a0fba6df2fa90
Martin Ayasse <martinay@outlook.com>
2017-10-22 21:37:29 +0000

    hands gestures

---
+++ Assets/VIEN/Scripts/GameLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+	public GameObject DrawingHud;
+
+	public void EnterDrawMode()
+	{
+		DrawingHud.SetActive(true);
+	}	
+
+	public void ExitDrawMode()
+	{
+		DrawingHud.SetActive(false);
+	}
+}
---
+++ Assets/VIEN/Scripts/GesturesLogic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GesturesLogic : MonoBehaviour
+{
+    public GameLogic GameLogic;
+
+	public GameObject DrawingHand;
+
+    private bool _isDrawingMode;
+    private bool _isDrawing;
+
+	void Update()
+	{
+		if(_isDrawing)
+			Debug.Log(DrawingHand.transform.TransformVector(Vector3.one));
+	}
+
+    public void OnPinchActivate()
+    {
+		if(!_isDrawingMode)
+			return;
+
+        _isDrawing = true;
+    }
+
+    public void OnPinchDeactivate()
+    {
+		if(!_isDrawingMode)
+			return;
+
+        _isDrawing = false;
+    }
+
+    public void OnFingerExtendActivate()
+    {
+		if(_isDrawingMode)
+			return;
+
+        _isDrawingMode = true;
+        GameLogic.EnterDrawMode();
+    }
+
+    public void OnFingerExtendDeactivate()
+    {
+		if(!_isDrawingMode)
+			return;
+
+        _isDrawingMode = false;
+        GameLogic.ExitDrawMode();
+    }
+}
