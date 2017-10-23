c200fc3ef040d5ad132b48715b4fbe1c6d2fceb5
Martin Ayasse <martinay@outlook.com>
2017-10-23 13:52:04 +0000

    gestures

--- Assets/VIEN/Scripts/GameLogic.cs
+++ Assets/VIEN/Scripts/GameLogic.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class GameLogic : MonoBehaviour {
 	public GameObject DrawingHud;
+	public GameObject DrawingSurface;
 
 	public void EnterDrawMode()
 	{
 		DrawingHud.SetActive(true);
+		DrawingSurface.SetActive(true);
 	}	
 
 	public void ExitDrawMode()
 	{
 		DrawingHud.SetActive(false);
+		DrawingSurface.SetActive(false);
 	}
 }
--- Assets/VIEN/Scripts/GesturesLogic.cs
+++ Assets/VIEN/Scripts/GesturesLogic.cs
@@ -1,20 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Leap.Unity;
 using UnityEngine;
 
 public class GesturesLogic : MonoBehaviour
 {
     public GameLogic GameLogic;
+    public GameObject LineRenderTarget;
 
-    public GameObject DrawingHand;
+    public IHandModel DrawingHand;
 
     private bool _isDrawingMode;
     private bool _isDrawing;
+    private Vector3 _lastPosition;
+    private LineRenderer _lineRenderer;
+
+    void Start()
+    {
+        _lineRenderer = LineRenderTarget.GetComponent<LineRenderer>();
+    }
 
     void Update()
     {
-        if (_isDrawing)
-            Debug.Log(DrawingHand.transform.position);
+        if (!_isDrawing)
+            return;
+        Vector3 positionScreen = GetHandPositionOnScreen();
+
+        var distanceToOld = Vector3.Distance(_lastPosition, positionScreen);
+        if (distanceToOld < 2)
+            return;
+
+        _lastPosition = positionScreen;
+        DrawNewPoint(positionScreen);
+    }
+
+    private Vector3 GetHandPositionOnScreen()
+    {
+        var hand = DrawingHand.GetLeapHand();
+        var position = ((hand.Fingers[0].TipPosition + hand.Fingers[1].TipPosition) * .5f).ToVector3();
+        var positionScreen = Camera.main.WorldToScreenPoint(hand.Fingers[0].TipPosition.ToVector3());
+        return positionScreen;
+    }
+
+    private void DrawNewPoint(Vector3 positionScreen)
+    {
+        var positionCount = _lineRenderer.positionCount;
+        _lineRenderer.SetVertexCount(positionCount ++);
+        var random = new System.Random();
+        _lineRenderer.SetPosition(_lineRenderer.positionCount, new Vector3(random.Next(5), random.Next(5), 0));
     }
 
     public void OnPinchActivate()
@@ -35,6 +69,7 @@ public class GesturesLogic : MonoBehaviour
 
     public void OnFingerExtendActivate()
     {
+        Init();
         if (_isDrawingMode)
             return;
 
@@ -49,5 +84,10 @@ public class GesturesLogic : MonoBehaviour
 
         _isDrawingMode = false;
         GameLogic.ExitDrawMode();
-    }    
+    }
+
+    private void Init()
+    {
+        _lastPosition = Vector3.zero;
+    }
 }
