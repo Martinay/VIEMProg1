47c3f1407a29c6ebc26b1d757bb26e53db57d9fd
pase13voxi <pascal_bechtoldt@web.de>
2017-10-24 17:46:34 +0000

    - Unnötige Pakete Entfernt - Alle Lampen gesetzt - Alle Wände haben eine Texture

---
+++ Assets/VIEN/Scripts/WorldBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBehavior : MonoBehaviour {
+	public GameObject Light0, Light1, Light2, Light3, Light4, Light5, Light6, Light7, Light8, Light9, Light10, Light11, Light12, Light13, Light14;
+	public AudioClip LightsOn;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartSequence (){
+		
+	}
+}
