71494bb5c8e6288cd436daacc6deb7aa1fd74a1c
pase13voxi <pascal_bechtoldt@web.de>
2017-10-26 15:51:57 +0000

    All Objects are placed but not finally.

---
+++ Assets/VIEN/Scripts/DrawableBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawableBehavior : MonoBehaviour {
+	public string SearchTag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Teleport() {
+		
+	}
+}
