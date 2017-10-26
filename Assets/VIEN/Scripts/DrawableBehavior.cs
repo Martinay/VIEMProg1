77be28cf34fbcd9aead1e6364f22d6379c1885de
pase13voxi <pascal_bechtoldt@web.de>
2017-10-26 16:31:05 +0000

    teleport should work ;) we will see!

--- Assets/VIEN/Scripts/DrawableBehavior.cs
+++ Assets/VIEN/Scripts/DrawableBehavior.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawableBehavior : MonoBehaviour {
+	public GameObject WorldInteraction;
+	public int State;
 	public string SearchTag;
+	private GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.FindWithTag("Player");
 	}
 	
 	// Update is called once per frame
@@ -15,6 +18,7 @@ public class DrawableBehavior : MonoBehaviour {
 	}
 
 	void Teleport() {
-		
+		player.transform.position = this.transform.position;
+		WorldInteraction.SendMessage ("SetState",State);
 	}
 }
