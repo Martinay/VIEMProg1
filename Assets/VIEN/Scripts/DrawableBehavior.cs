26cc77d2baa51c31b69509bdb51581c4e0d3bb4a
pase13voxi <pascal_bechtoldt@web.de>
2017-11-06 12:23:32 +0000

    Fehler behoben

--- Assets/VIEN/Scripts/DrawableBehavior.cs
+++ Assets/VIEN/Scripts/DrawableBehavior.cs
@@ -7,19 +7,35 @@ public class DrawableBehavior : MonoBehaviour {
 	public int State;
 	public string[] SearchTag = new string[1];
 	private GameObject player;
+	private float time = 0.0f;
+	private bool start;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag("Player");
+		start = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Timing();
+	}
+
+	public void Timing() {
+		if (start) {
+			time += Time.deltaTime;
+			if (time >= 3.0f) {
+				newPosition ();
+				start = false;
+			}
+		}
 	}
 
 	public void Teleport() {
+		start = true;
 		WorldInteraction.SendMessage ("PlayBanana");
-		System.Threading.Thread.Sleep (3000);
+	}
+
+	private void newPosition() {
 		player.transform.position = this.transform.position;
 		player.transform.rotation = this.transform.rotation;
 		WorldInteraction.SendMessage ("SetState",State);
