5a1978d65aaa719832fb6d1c2c6b4deb1b04c780
pase13voxi <pascal_bechtoldt@web.de>
2017-11-06 18:58:10 +0000

    Debug Mode hinzugefügt

---
+++ Assets/VIEN/Scripts/Presentation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Presentation : MonoBehaviour {
+	public GameObject Enemy;
+	private bool pauseGame = false;
+	private bool pauseEnemy = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("p")) {
+			if (!pauseGame)
+				Time.timeScale = 0;
+			else
+				Time.timeScale = 1;
+			pauseGame = !pauseGame;
+		}
+
+		if (Input.GetKeyDown ("o")) {
+			Enemy.SetActive(pauseEnemy);
+			pauseEnemy = !pauseEnemy;
+		}
+	}
+}
